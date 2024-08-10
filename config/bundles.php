4e0ff3111a8420e067a07e9850934fb35ff4677e
Lukiashchenko Dmytrii <lukiaschenkoff@gmail.com>
2024-08-10 03:39:44 +0200

    Configs Set Up

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
